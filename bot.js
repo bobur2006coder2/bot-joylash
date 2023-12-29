50c6838e89c3a410d8ffc0093d5e4896bc0ccec2
bobur2006coder <boburdilmurodov702@gmail.com>
2023-12-29 19:17:14 +0500

    fix

--- bot.js
+++
@@ -1,8 +0,0 @@
-const { Telegraf } = require("telegraf")
-const bot = new Telegraf("6653286311:AAEtzsppeLVsUMPiKGs8MEi5xLgl5SQAMaI")
-
-bot.start(ctx => {
-    ctx.reply(`salom dunyo`)
-})
-
-bot.launch();
